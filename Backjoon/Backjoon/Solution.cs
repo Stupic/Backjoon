dd1853ea37665e8942277d0fbad1275e8e073273
stupic0713 <stupic0713@gmail.com>
2020-07-09 13:07:07 +0900

    P1011

--- Backjoon/Backjoon/Solution.cs
+++ Backjoon/Backjoon/Solution.cs
@@ -1,8 +1,50 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Threading;
 
 public class Solution
 {
+    public void P1011()
+    {
+
+        int T = Convert.ToInt32(Console.ReadLine());
+        
+        for(int j = 0 ; j<T; j++)
+        {
+            String arg = Console.ReadLine();
+            String[] XY = arg.Split(" ");
+            long X = Convert.ToInt64(XY[0]);
+            long Y = Convert.ToInt64(XY[1]);
+            int mod = 0; // 자리수 짝수= 0,홀수= 1  11 111 121 1211 1221 12121 12221 12321
+            int add = 1;  // 증가값
+            int move = 2;
+            int i = 2;
+
+            while (i < Y-X)
+            {
+                if (mod == 0)
+                {
+                    add++;
+                    i = i + add;
+                    mod = 1;
+                    move++;
+                }
+                else if (mod == 1)
+                {
+                    i = i + add;
+                    mod = 0;
+                    move++;
+                }
+            }
+            if (Y-X == 1)
+                move = 1;
+
+            Console.WriteLine(move);
+        }
+        
+   
+
+    }
     public void P2775() 
     {
         // 버전차이로 컴파일 에러 , 메서드 안에 메서드 있어서 오류
