f0fb3bad71c3d1bd47b71ea9d569851d32f37ed2
신봉석 <stupic0713@gmail.com>
2024-08-19 10:24:24 +0900

    백준 10699번 오늘 날짜 코드

---
+++ Java/src/P10699/Main.java
@@ -0,0 +1,14 @@
+package P10699;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+
+    }
+
+}
