b73ebc950c2e6c85e783e2f83491b2387e482c57
신봉석 <stupic0713@gmail.com>
2024-09-04 13:19:46 +0900

    백준 2164번 카드2

---
+++ Java/src/P2164/Main.java
@@ -0,0 +1,28 @@
+package P2164;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//규칙:       *       *               *                               *
+//입력:   1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16
+//결과:   1	2	2	4	2	4	6	8	2	4	6	8	10	12	14	16
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(br.readLine());
+        if(size == 1){
+            System.out.println(1);
+        }else{
+            int maxN = 2;
+            while (maxN < size) {
+                maxN *= 2;
+            }
+            int reverseIndex = maxN - size;
+            System.out.println(maxN - reverseIndex * 2);
+        }
+        br.close();
+    }
+}
