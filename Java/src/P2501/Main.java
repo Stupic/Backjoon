3125469bbf83cb431d84aa57c030aa56ce4b6d1f
신봉석 <stupic0713@gmail.com>
2024-09-04 09:33:10 +0900

    백준 2501번 약수 구하기

---
+++ Java/src/P2501/Main.java
@@ -0,0 +1,21 @@
+package P2501;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int p = Integer.parseInt(input[0]);
+        int q = Integer.parseInt(input[1]);
+        int seq = 0 , i ;
+        for (i = 1; i <= p; i++) {
+            if (p % i == 0) seq++;
+            if (seq == q) break;
+        }
+        System.out.println(seq == q ? i : 0);
+        br.close();
+    }
+}
