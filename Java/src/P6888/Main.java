20e75fd6be4bdbb4d7f14b9d74e6c2af457e7241
신봉석 <stupic0713@gmail.com>
2024-12-10 09:27:23 +0900

    백준 (6888) Terms of Office

---
+++ Java/src/P6888/Main.java
@@ -0,0 +1,22 @@
+package P6888;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int start = Integer.parseInt(br.readLine());
+        int end = Integer.parseInt(br.readLine());
+
+        for (int y = start; y <= end; y +=  3 * 4 * 5) {
+            System.out.println("All positions change in year 1" + y);
+        }
+
+        br.close();
+    }
+}
+
